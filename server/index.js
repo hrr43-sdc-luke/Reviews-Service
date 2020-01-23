fce9486a60eeb787c0b60f27979d474a9cf290f2
Dan Moy <danmoy69@gmail.com>
2020-01-23 15:37:01 -0800

    setup server - forgot to branch

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3007;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+app.listen(port, () => { console.log(`Listening on port ${port}`)})
