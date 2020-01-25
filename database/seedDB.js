e747eab84d99ab6424cffaa062ac0820ef187d8d
Dan Moy <danmoy69@gmail.com>
2020-01-24 17:33:41 -0800

    cleared out some comments

--- database/seedDB.js
+++ database/seedDB.js
@@ -4,27 +4,7 @@ const fakerData = require('./fakerData.js');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-//write a function to insert all of the faker data into the DB
-
 const sampleData = fakerData.createFakeData()
-// [
-//   {
-//     experience_id: 4,
-//     username: 'Bob',
-//     avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/adamnac/128.jpg',
-//     date: '20-01-01',
-//     stars: 5,
-//     review: 'blah blibidy blah'
-//   },
-//   {
-//     experience_id: 4,
-//     username: 'Jane',
-//     avatar:  'https://s3.amazonaws.com/uifaces/faces/twitter/millinet/128.jpg',
-//     date: '20-02-01',
-//     stars: 4,
-//     review: 'blah blibidy blah blah blah'
-//   }
-// ]
 
 const seedDB = () => {
   for (var i = 0; i < sampleData.length; i++) {
