434c941f28815f0459fc6e5fce83c6d75f0f5f3f
Dan Moy <danmoy69@gmail.com>
2020-01-29 15:18:27 -0800

    cleaning up before taking a break from jest testing. commented out the apiCalls.test and App.test tests

--- client/__tests__/App.test.js
+++ client/__tests__/App.test.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import Enzyme from 'enzyme';
-import { shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16.1';
-import App from '../components/App.jsx';
+// import React from 'react';
+// import Enzyme from 'enzyme';
+// import { shallow } from 'enzyme'
+// import Adapter from 'enzyme-adapter-react-16.1';
+// import App from '../components/App.jsx';
+// import Window from 'window';
 
-Enzyme.configure({ adapter: new Adapter() })
+// const window = new Window();
 
-describe("App", () => {
-  const wrapper = shallow(<App />)
-  it('App should render aggregate star rating', () => {
-    const rating = wrapper.find('.aggRating');
-    expect(rating).toExist();
-  });
+// Enzyme.configure({ adapter: new Adapter() })
 
-})
+// describe("App", () => {
+//   const wrapper = shallow(<App />)
+//   it('App should render aggregate star rating', () => {
+//     const rating = wrapper.find('.aggRating');
+//     expect(rating).toExist();
+//   });
+
+// })
--- client/__tests__/apiCalls.test.js
+++ client/__tests__/apiCalls.test.js
@@ -1,27 +1,10 @@
-const apiCalls = require('../apiCalls.js');
-const request = require('supertest');
-const express = require('express');
+// const apiCalls = require('../apiCalls.js');
+// const request = require('supertest');
+// const express = require('express');
 
 
-describe('getReviews API', () => {
-  // const expected = [
-  //   {
-  //       "id": 112,
-  //       "experience_id": 5,
-  //       "username": "Cicero",
-  //       "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/steynviljoen/128.jpg",
-  //       "date": "2020-02-01T08:00:00.000Z",
-  //       "stars": 3,
-  //       "review": "Eos odit et ad."
-  //   }
-  // ];
-  // it('getReviews with an experiece id of 5 should return a array with an object.experience_id of 5', (done) => {
-  //   request(app)
-  //   .get('./reviews/5')
-  //   // .set('Accept', 'application/json')
-  //   .then((response) => {
-  //     expect(response.body[0].experience_id).toBe(5)
-  //     done();
-  //   })
-  // });
-});
+// describe('getReviews API', () => {
+//   it('getReviews with an experiece id of 5 should return a array with an object.experience_id of 5', (done) => {
+
+//   });
+// });
