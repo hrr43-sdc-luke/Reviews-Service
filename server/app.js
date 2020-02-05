c4c1ddb57a4dd7a93f44be4733bce8298d219339
Dan Moy <danmoy69@gmail.com>
2020-02-04 22:18:54 -0800

    Got it working with proxy server-updated my axios url to the full url, added default expId of 1; organized some of the server code

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const ejs = require('ejs');
 const db = require('../database/index.js');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3006',
+  optionsSuccessStatus: 200,
+}));
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.engine('html', ejs.renderFile);
+
+app.get('/:id', (req, res) => {
+  res.render('../public/index.html');
+});
 
 app.get('/reviews/:id', (req, res) => {
   db.getExpReviews(req.params.id, (err, reviews) => {
@@ -17,6 +28,5 @@ app.get('/reviews/:id', (req, res) => {
     }
   });
 });
-
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,5 @@
 const app = require('./app.js');
 
-app.engine('html', require('ejs').renderFile);
-
 const port = process.env.PORT || 3007;
-
-app.get('/:id', (req, res) => {
-  res.render('../public/index.html');
-});
 
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
