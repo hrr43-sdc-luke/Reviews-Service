ed86d649d40fbb477860126761b06907d0874142
Dan Moy <danmoy69@gmail.com>
2020-02-08 22:50:52 -0800

    adding more to the cors exception

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const db = require('../database/index.js');
 
 const app = express();
 
-const whitelist = ['http://18.222.165.232:4000', 'http://18.222.165.232:3007', 'http://18.223.132.12:4000', 'http://18.217.113.225:4000', 'http://localhost:4000'];
+const whitelist = ['http://18.222.165.232:4000', 'http://18.222.165.232:3007', 'http://18.223.132.12:4000', 'http://18.217.113.225:4000', 'http://localhost:3007', 'http://localhost:4000'];
 const corsOptions = {
   origin: (origin, callback) => {
     if (whitelist.indexOf(origin) !== -1) {
