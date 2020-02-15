36e30dc15847cda77734ac07270071ae24c7ad80
Phil <pronewgen92@gmail.com>
2020-02-15 11:22:05 -0500

    Best copy of data generation.

--- README.md
+++ README.md
@@ -13,9 +13,17 @@ To set up the repo go [here](REVIEWS-SERVICE.md).
 * [`PUT`](server/app.js#L47) - Update a review
 * [`DELETE`](server/app.js#L57) - Delete a review
 
-## Environment Variables
+## Generate Data
 
-#### Postgres
+* First you will need to set up with a `MOCK-DATA.json` file. This should be an array of objects and each object should have these three keys, with fake data value: `userName`,`review`, and `date (yyyy-mm-dd)`
+* The avatar url is fetched via faker. However you can include an `avatar` key with an img url to the MOCK-DATA.json file. If you choose this option then you will need to take out this [line](generateData/index.js#L26), which is the avatar variable. Instead you will add the avatar in the object destructuring [here](generateData/index.js#L23-25).
+* You can edit the amount of records you want to create [here](generateData/index.js#L11-12), you can also name your csv data file [here](generateData/index.js#L7). Once satisfied you can run the data generator with `node generateData/`
+
+## Databases (environment variables/schema)
+
+### Postgres
+
+#### Environment variables
 
 * `PGUSER` - postgres
 * `PGHOST` - localhost
@@ -23,6 +31,6 @@ To set up the repo go [here](REVIEWS-SERVICE.md).
 * `PGDATABASE` - postgres
 * `PGPORT` - 5432
 
-#### Insert schema into postgres
+#### Insert schema (be sure to have postgres service running)
 
- `psql -h localhost -U postgres -f database/postgres_schema.sql`
+`psql -h localhost -U postgres -f database/postgres_schema.sql`
--- generateData/index.js
+++ generateData/index.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 const hrtime = require('process.hrtime');
 
 const timer = hrtime();
+const mockData = require('./MOCK-DATA.json');
 
-const writeUsers = fs.createWriteStream('database/lrg-data.csv');
+const dataFileName = 'data';
+
+const writeUsers = fs.createWriteStream(`generateData/${dataFileName}.csv`);
 writeUsers.write('id,experience_id,username,avatar,date,stars,review\n', 'utf8');
 
 const noPrimaryRecords = 10000000; // goal is to get 10mil experience records
@@ -17,11 +20,14 @@ function writeTenMillionUsers(writer, encoding, callback) {
     let ok = true;
     for (let i = reviewId; i < totalRecords && ok; i += 1) {
       reviewId += 1;
-      const experienceId = Math.floor(Math.random() * noPrimaryRecords);
-      const starMinMax = { min: 0, max: 5 };
-      const date = faker.date.recent(90);
-      const strDate = JSON.stringify(date).split('T')[0].slice(1);
-      const reviewData = `${reviewId},${experienceId},${faker.internet.userName()},${faker.image.avatar()},${strDate},${faker.random.number(starMinMax)},${faker.lorem.paragraph()}\n`;
+      const itemData = mockData[Math.floor(Math.random() * mockData.length)];
+      const {
+        userName, date, review,
+      } = itemData;
+      const avatar = faker.image.avatar()
+      const experienceId = Math.floor(Math.random() * noPrimaryRecords) + 1;
+      const starNo = Math.floor(Math.random() * (5 - 0 + 1));
+      const reviewData = `${reviewId},${experienceId},${userName},${avatar},${date},${starNo},${review}\n`;
       if (i === totalRecords - 1) {
         writer.write(reviewData, encoding, callback);
       } else {
@@ -37,5 +43,5 @@ function writeTenMillionUsers(writer, encoding, callback) {
 
 writeTenMillionUsers(writeUsers, 'utf-8', () => {
   writeUsers.end();
-  console.log(`CSV file created. It took ${hrtime(timer, 's')}seconds to complete`);
+  console.log(`${dataFileName}.csv file created. It took ${hrtime(timer, 's')}seconds to complete`);
 });
