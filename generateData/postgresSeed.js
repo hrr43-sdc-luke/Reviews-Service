ed99b7755bb3a8a33af9e9072608163c5580c3b7
Phil <pronewgen92@gmail.com>
2020-02-15 19:59:35 -0500

    pgdb schema/seed.

--- generateData/postgresSeed.js
+++ generateData/postgresSeed.js
@@ -2,11 +2,11 @@ const { Client } = require('pg');
 
 require('dotenv').config();
 
-const user = process.env.PGUSER
-const host = process.env.PGHOST
-const database = process.env.PGDATABASE
-const password = process.env.PGPASSWORD
-const port = process.env.PGPORT
+const user = process.env.PGUSER;
+const host = process.env.PGHOST;
+const database = process.env.PGDATABASE;
+const password = process.env.PGPASSWORD;
+const port = process.env.PGPORT;
 const connectionString = `postgres://${user}:${password}@${host}:${port}/${database}`;
 const client = new Client({ connectionString });
 
