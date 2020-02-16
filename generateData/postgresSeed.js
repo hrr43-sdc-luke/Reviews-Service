0e5fff79be8625cf592a0e9da3b3ef16f8fae6f2
Phil <pronewgen92@gmail.com>
2020-02-15 19:59:13 -0500

    pgdb schema/seed.

--- generateData/postgresSeed.js
+++ generateData/postgresSeed.js
@@ -1,21 +1,15 @@
 const { Client } = require('pg');
-const cp = require('child_process');
-const hrtime = require('process.hrtime');
-
-const cmdLine = 'psql -h localhost -U postgres -f generateData/postgresSchema.sql';
 
 require('dotenv').config();
 
 const user = process.env.PGUSER
 const host = process.env.PGHOST
 const database = process.env.PGDATABASE
-console.log(database)
 const password = process.env.PGPASSWORD
 const port = process.env.PGPORT
 const connectionString = `postgres://${user}:${password}@${host}:${port}/${database}`;
 const client = new Client({ connectionString });
 
-const timer = hrtime();
 client.connect();
 client.on('connect', () => {
   console.log('Postgres is connected');
