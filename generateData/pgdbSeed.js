475217162da8e3492c2871bc70d75092f79b76a8
Phil <pronewgen92@gmail.com>
2020-02-22 00:46:15 -0500

    COPY INDEX after seeding is faster according to the docs!

--- generateData/pgdbSeed.js
+++ generateData/pgdbSeed.js
@@ -14,24 +14,25 @@ let timer;
 client.connect((err, pgdb) => {
   console.log('Connected to postgres. Please wait a moment while your records are being copied...');
   timer = hrtime();
-  pgdb.query('Create index experience_idx on reviews using hash (experience_id);', (indexErr) => {
-    if (indexErr) {
-      console.error(err);
-    } else {
-      console.log('Index created for experience_id...');
-      const stream = pgdb.query(copyFrom('COPY reviews(id,experience_id,username,review,date,stars,avatar) FROM STDIN CSV HEADER;'));
-      const fileStream = fs.createReadStream(filePath);
-      fileStream.on('error', (error) => {
-        console.log(`Error in reading file: ${error}`);
-      });
-      stream.on('error', (error) => {
-        console.log(`Error in copy command: ${error}`);
-      });
-      stream.on('end', () => {
-        console.log(`Success! ${csvFile} was copied into postgres in ${hrtime(timer, 's')}seconds`);
-        client.end();
-      });
-      fileStream.pipe(stream);
-    }
+  const stream = pgdb.query(copyFrom('COPY reviews(id,experience_id,username,review,date,stars,avatar) FROM STDIN CSV HEADER;'));
+  const fileStream = fs.createReadStream(filePath);
+  fileStream.on('error', (error) => {
+    console.log(`Error in reading file: ${error}`);
   });
+  stream.on('error', (error) => {
+    console.log(`Error in copy command: ${error}`);
+  });
+  stream.on('end', () => {
+    console.log(`Success! ${csvFile} was copied into postgres in ${hrtime(timer, 's')}seconds`);
+    console.log('Please wait a little more while the index for experience_id is being created')
+    pgdb.query('Create index experience_idx on reviews using hash (experience_id);', (indexErr) => {
+      if (indexErr) {
+        console.error(err);
+      } else {
+        console.log('Index created for experience_id! All Done!');
+        client.end();
+      }
+    });
+  });
+  fileStream.pipe(stream);
 });
